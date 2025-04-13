de741349052410be6ede9db909493cffa9b2e2ce
abatista-planas <albgzz@mst.edu>
2025-04-13 17:08:39 -0500

    feat: Adding a toy model

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.pes_1D.predictor import Predictor
+
+if __name__ == "__main__":
+    predictor = Predictor("./src/")
+    y = predictor.predict_one([1.2, 0.8, 2.4, 1.2])
+    print(y)
+
+    y_batch = predictor.predict_batch([[1.2, 0.8, 2.4, 1.2], [2.1, 1.4, 1.0, 3.2]])
+    print(y_batch)
