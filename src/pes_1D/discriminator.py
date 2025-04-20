c1e80a9d11cfb966dcaeaf4ea8222c87e86ac93c
abatista-planas <albgzz@mst.edu>
2025-04-20 07:42:27 -0500

    refactor: parameters will be given in a dictionary, instead of individual parameters

--- src/pes_1D/discriminator.py
+++ src/pes_1D/discriminator.py
@@ -4,31 +4,37 @@ from torchsummary import summary  # type: ignore
 
 
 class AnnDiscriminator(nn.Module):
-    def __init__(self, in_features=4, hidden_layers: list[int]= [2,2,2,2], out_features=2):
+    def __init__(self, model_paramaters):
         super(AnnDiscriminator, self).__init__()
 
         self.layers = nn.ModuleDict()
         # input layer
-        self.layers["input"] = nn.Linear(in_features, hidden_layers[0])
-        
-        # hidden layers
-        for i in range(len(hidden_layers)-1):
-            self.layers[f"hidden_{i}"] = nn.Linear(hidden_layers[i], hidden_layers[i + 1])
+        self.layers["input"] = nn.Linear(
+            model_paramaters["in_features"], model_paramaters["hidden_layers"][0]
+        )
 
-        
-        self.layers['output'] = nn.Linear(hidden_layers[-1], out_features)
+        # hidden layers
+        for i in range(len(model_paramaters["hidden_layers"]) - 1):
+            self.layers[f"hidden_{i}"] = nn.Linear(
+                model_paramaters["hidden_layers"][i],
+                model_paramaters["hidden_layers"][i + 1],
+            )
+
+        self.layers["output"] = nn.Linear(
+            model_paramaters["hidden_layers"][-1], model_paramaters["out_features"]
+        )
         pass
 
     def forward(self, x):
         # input layer
         x = F.relu(self.layers["input"](x))
-        
+
         # hidden layers
         for i in range(len(self.layers) - 2):
             x = F.relu(self.layers[f"hidden_{i}"](x))
-        
+
         # output layer
-        x = self.layers['output'](x)
+        x = self.layers["output"](x)
         return x
 
     def summary(self, input_size):
@@ -37,18 +43,18 @@ class AnnDiscriminator(nn.Module):
 
 # class AnnDiscriminator(nn.Module):
 #     def __init__(self, in_features=4, hidden_layers: list[int]= [2,2,], out_features=2):
-        
+
 #         super(AnnDiscriminator, self).__init__()
-        
+
 #         self.layers = nn.ModuleDict()
-        
+
 #         # # input layer
 #         # self.layers["input"] = nn.Linear(in_features, hidden_layers[0])
 #         # # hidden layers
-        
+
 #         # for i in range(len(hidden_layers) - 1):
-#         #     self.layers[f"hidden_{i}"] = nn.Linear(hidden_layers[i], hidden_layers[i + 1])    
-            
+#         #     self.layers[f"hidden_{i}"] = nn.Linear(hidden_layers[i], hidden_layers[i + 1])
+
 #         # # output layer
 #         # self.layers["output"] = nn.Linear(hidden_layers[-1], out_features)
 
@@ -74,6 +80,5 @@ class AnnDiscriminator(nn.Module):
 
 #         # return output layer
 #         x = self.layers['output'](x)
-        
-#         return x
 
+#         return x
