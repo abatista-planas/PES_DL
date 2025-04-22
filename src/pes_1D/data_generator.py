9a5f0e52f13d2dd32d32317761d0c92d397bb48a
abatista-planas <albgzz@mst.edu>
2025-04-22 08:28:22 -0500

    refactor: generalize the true pes generators

--- src/pes_1D/data_generator.py
+++ src/pes_1D/data_generator.py
@@ -1,7 +1,6 @@
 """Generates sythetic data and noised data for the Lennard-Jones model"""
 
 from typing import Tuple
-
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
@@ -14,6 +13,7 @@ from pes_1D.utils import NoiseFunctions, PesModels
 def generate_discriminator_training_set(
     n_samples: int,
     size: int,
+    pes_name_list: list[str] = ["lennard_jones"],
     properties_list: list[str] = ["energy"],
     deformation_list: npt.NDArray[np.str_] = np.array(["outliers", "oscillation"]),
     properties_format: str = "table",
@@ -22,12 +22,12 @@ def generate_discriminator_training_set(
     generator_seed: list[int] = [37, 43],
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, pd.DataFrame]:
     """Generates training sets for the Lennard-Jones potential"""
+    df_good_sample = generate_true_pes_samples(
+    pes_name_list, int(n_samples / 2), size, generator_seed[0]
+    ) 
 
-    df_good_sample = generate_lennard_jones_samples(
-        int(n_samples / 2), size, seed=generator_seed[0]
-    )
-    df_bad_sample = generate_bad_lennard_jones_samples(
-        n_samples - int(n_samples / 2), size, deformation_list, seed=generator_seed[1]
+    df_bad_sample = generate_bad_samples(
+        pes_name_list, n_samples - int(n_samples / 2), size, deformation_list, seed=generator_seed[1]
     )
 
     df_all = pd.concat([df_good_sample, df_bad_sample])
@@ -63,17 +63,61 @@ def generate_discriminator_training_set(
         df_all,
     )
 
+def generate_discriminator_training_set_from_df(
+    df_all: pd.DataFrame,
+    properties_list: list[str] = ["energy"],
+    properties_format: str = "table",
+    test_split: float = 0.2,
+    gpu: bool = True,
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, pd.DataFrame]:
+    """Generates training sets for the Lennard-Jones potential"""
 
-def generate_bad_lennard_jones_samples(
+    n_samples = len(df_all)
+    # Shuffle DataFrame in place
+    df_all = df_all.sample(frac=1).reset_index(drop=True)
+
+    
+    label_tensor = torch.tensor(df_all["true_pes"]).flatten()
+
+    def input_format(pes):
+        """Define input format"""
+        if properties_format == "array":
+            return pes[properties_list].values.flatten()
+        else:
+            return pes[properties_list].values
+
+    # Convert continuous variables to a tensor
+    input_arrays = [input_format(pes) for pes in df_all["pes"].values]
+    input_stack = np.stack(input_arrays)
+    input_tensor = torch.tensor(input_stack, dtype=torch.float)
+
+    test_size = int(n_samples * test_split)  # test_split% for testing
+
+    input_train = input_tensor[: n_samples - test_size]
+    input_test = input_tensor[n_samples - test_size : n_samples]
+    label_train = label_tensor[: n_samples - test_size]
+    label_test = label_tensor[n_samples - test_size : n_samples]
+
+    return (
+        input_train.cuda() if gpu else input_train,
+        label_train.cuda() if gpu else label_train,
+        input_test.cuda() if gpu else input_test,
+        label_test.cuda() if gpu else label_test,
+        df_all,
+    )
+
+
+def generate_bad_samples(
+    pes_name_list: list[str],
     n_samples: int,
     size: int,
     deformation_list: npt.NDArray[np.str_] = np.array(["outliers", "oscillation"]),
     seed: int = 33,
-):
+)-> pd.DataFrame:
     np.random.seed(seed)
 
     """Generates a set of good samples from the Lennard-Jones potential with noise"""
-    df_bad_sample = generate_lennard_jones_samples(n_samples, size, seed)
+    df_bad_sample = generate_true_pes_samples(pes_name_list,n_samples, size, seed)
 
     # Randomly select deformation types for each sample
     deformed_list = deformation_list[
@@ -82,7 +126,7 @@ def generate_bad_lennard_jones_samples(
 
     # Apply the deformation functions
     def assign_deformation_type(row):
-        row.model_type = "twisted_lennard_jones"
+        row.model_type = "twisted_pes"
         row.true_pes = 0
         row.modified_pes = 1
         row.deformation_type = deformed_list[row.name]
@@ -93,6 +137,7 @@ def generate_bad_lennard_jones_samples(
             )
             row.pes["energy"] += outliers_function
             row.pes["derivative"] += outliers_derivative
+            
         elif deformed_list[row.name] == "oscillation":
             r0 = np.random.uniform(0.1, 1.5)
             A = np.random.uniform(1, 3)
@@ -123,16 +168,93 @@ def generate_bad_lennard_jones_samples(
                     if max_min_diff > 0
                     else row.pes[col] - row.pes[col].min()
                 )
+        elif deformed_list[row.name] == "random_functions":
+            # Generate random function
+            r = np.linspace(0.1, 10, size, dtype=np.float64)
+            fn_label,fn,dfn = NoiseFunctions.random_function(r)
+            
+            row.model_type = fn_label
+            row.pes["r"] = r
+            row.pes["energy"] = fn
+            row.pes["derivative"] = dfn
+            row.pes["inverse_derivative"] = 1.0 / (dfn + 1e-10)
 
+            # Normalize dataframe
+            for col in row.pes.columns:
+                max_min_diff = row.pes[col].max() - row.pes[col].min()
+                row.pes[col] = (
+                    (row.pes[col] - row.pes[col].min()) / (max_min_diff)
+                    if max_min_diff > 0
+                    else row.pes[col] - row.pes[col].min()
+                )
+                        
         return row
 
     df_bad_sample = df_bad_sample.apply(assign_deformation_type, axis=1)
 
     return df_bad_sample
 
+def generate_true_pes_samples(
+    pes_name_list: list[str], n_samples: int, size: int, seed: int=33
+) -> pd.DataFrame:
+    """Generates a set of samples"""
 
-def generate_lennard_jones_samples(
-    n_samples: int, size: int, seed: int = 33
+    if len(pes_name_list) == 0:
+   
+        return pd.DataFrame(
+                {
+                    "model_type": [""] * n_samples,
+                    "true_pes": [0] * n_samples,
+                    "parameters": [{}] * n_samples,
+                    "pes": [pd.DataFrame()]*n_samples,
+                    "modified_pes": [0] * n_samples,
+                    "deformation_type": [""] * n_samples,
+                    "deformation_parameters": [{}] * n_samples,
+                }
+            )
+   
+    
+    sample_count = n_samples
+    df_pes = pd.DataFrame()
+    
+    
+    
+    for pes_name in pes_name_list:
+        if sample_count < 1:
+            break
+        if pes_name == "lennard_jones":
+            parameters_array  = np.zeros((n_samples,2))
+            parameters_array[:,0]= np.random.uniform(1.2, 10.0, n_samples)
+            parameters_array[:,1]= np.random.uniform(5, 2000, n_samples)
+            df = generate_analytical_pes_samples(pes_name,parameters_array,size, seed)
+            sample_count = 0
+            
+        elif pes_name == "reudenberg":
+            number_of_samples = min(sample_count, len(PesModels.reudenberg_parameters))
+            keys = PesModels.reudenberg_parameters.keys()
+            reudenberg_keys = list(keys)[:number_of_samples]
+    
+            parameters_array = np.zeros((number_of_samples,len(PesModels.reudenberg_parameters[reudenberg_keys[0]]) ))
+            for i,key in enumerate(reudenberg_keys):
+                parameters_array[i,:] = PesModels.reudenberg_parameters[key]
+
+            sample_count = sample_count - number_of_samples
+            df = generate_analytical_pes_samples(pes_name,parameters_array,size, seed)
+            df["model_type"] = ["reudenberg_" + key for key in reudenberg_keys]
+        else:
+            
+            sample_count = 0
+            
+        df_pes =pd.concat([df_pes,df] ,axis=0, ignore_index=True)
+        
+    return df_pes    
+
+
+def generate_analytical_pes_samples(
+    pes_name: str ="lennard_jones",
+    parameters_array: npt.NDArray[np.float64] = np.array([]),
+    size: int = 128,
+    seed: int = 33,
 ) -> pd.DataFrame:
     """Generates a set of samples from the Lennard-Jones potential
 
@@ -144,38 +266,45 @@ def generate_lennard_jones_samples(
     Returns:
         pd.DataFrame: DataFrame containing the generated samples
     """
+    
     np.random.seed(seed)
-
-    sigma_array = np.random.uniform(1.2, 10.0, n_samples)
-    epsilon_array = np.random.uniform(5, 600, n_samples)
+    n_samples = parameters_array.shape[0]
     wall_max_high = np.random.uniform(1000, 2000, n_samples)
-    long_range_limit = np.random.uniform(0.05, 0.10, n_samples)
+    long_range_limit = np.random.uniform(0.01, 0.40, n_samples)
     df_samples = []
-
+    
+                
     for i in range(n_samples):
-        # Generate random parameters
-        sigma = sigma_array[i]
-        epsilon = epsilon_array[i]
-        r_0 = sigma * 2 ** (1 / 6)
+        # Get random parameters
+        parameters = parameters_array[i]
+        zero = getattr(PesModels, pes_name)(parameters, np.array(100))
+        
+        def pes(r):
+            return getattr(PesModels, pes_name)(parameters, r)-zero
+        
+        r_trial = np.linspace(0.1, 50.0, 500)
+        energy_array = pes(r_trial)
+        
+        min_index = np.argmin(energy_array)
+        r_0 = r_trial[min_index]
+        approx_well_depth = energy_array[min_index]
+        
         max_high = wall_max_high[i]
-
-        # Calculate the well depth
-        well_depth = PesModels.lennard_jones(sigma, epsilon, np.array([r_0]))
-        long_range_max = abs(long_range_limit[i] * well_depth)
+        long_range_max = abs(long_range_limit[i] * approx_well_depth)
 
         # Find Proper boundary: Using bisection (requires a bracketing interval)
         def f_min(r):
-            return PesModels.lennard_jones(sigma, epsilon, r) - max_high
+            return pes(r)- max_high
 
         r_min = optimize.bisect(f_min, 0.001, 100)
-
+       
         def f_max(r):
-            return abs(PesModels.lennard_jones(sigma, epsilon, r)) - long_range_max
+            return abs(pes(r)) - long_range_max
 
-        r_max = optimize.bisect(f_max, r_min, 100)
+        r_max = optimize.bisect(f_max, r_0, 100)
 
         # Generate samples
-        df = PesModels.lennard_jones_pes(sigma, epsilon, r_min, r_max, size)
+        df = PesModels.analytical_pes(pes_name, parameters, r_min, r_max, size)
 
         # Normalize dataframe
         for col in df.columns:
@@ -190,14 +319,11 @@ def generate_lennard_jones_samples(
 
     return pd.DataFrame(
         {
-            "model_type": ["lennard_jones"] * n_samples,
+            "model_type": [pes_name] * n_samples,
             "true_pes": [1] * n_samples,
             "parameters": [
-                {
-                    "sigma": sigma_array[i],
-                    "epsilon": epsilon_array[i],
-                }
-                for _ in range(n_samples)
+                {"parameters": parameters_array[i]}
+                for i in range(n_samples)
             ],
             "pes": df_samples,
             "modified_pes": [0] * n_samples,
